d78c0d7323273634289d3637e4e10e4278f77acd
Princer101 <31076774+Princer101@users.noreply.github.com>
2018-05-03 18:56:52 -0600

    Create EggTheif(Updated Not Tested).cpp

---
+++ EggTheif(Updated Not Tested).cpp
@@ -0,0 +1,100 @@
+/******************************\
+* Program: EggTheif.cpp  BETA  *
+* Creators: Tristan and Austin *
+* Last Edit: 5/03/18           *
+\******************************/
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+void Field(char field[60][31]){
+	int x = 0;
+	int y = 0;
+	ofstream land;
+	land.open("Eggland.txt", ios::out);
+	for(x = 0; x < 31; x++){
+		for(y = 0; y < 60; y++){
+			land << 'O';
+		}
+		land << endl;
+	}
+	land.close();
+}
+
+int main(int argc, char *argv[]) {
+	
+	char field[60][31] = {'O'};
+	char s = ' ';
+	int xPos = 0;
+	int yPos = 0;
+	system("open /Users/student/Documents/Egg-Theif_V0.2b.html");
+	
+	Field(field);
+	/*int x = 0;
+	int y = 0;
+	for(x = 0; x < 0; x++){
+		for(y = 0; y < 0; y++){
+			cout << field[x][y];
+		}
+		cout << endl;
+	}*/
+
+	
+	while(s != 'Q'){
+			system("stty raw");
+			s = getchar();
+			
+			
+			
+			if(s == 'W' || s =='w'){
+				cout << "You went up" << endl;
+				if(field[x][y] != 'O'){
+					y += 1;
+				}
+			}
+			if(s == 'A' || s =='a'){
+				cout << "You went left" << endl;
+				if(field[x][y] != 'O'){
+					x -= 1;
+				}
+			}
+			if(s == 'S' || s =='s'){
+				cout << "You went down" << endl;
+				if(field[x][y] != 'O'){
+					y -= 1;
+				}
+			}
+			if(s == 'D' || s =='d'){
+				cout << "You went right" << endl;
+				if(field[x][y] != 'O'){
+					x += 1;
+				}
+			}
+			if(x >= 60){
+				x = 0;
+			}
+			if(x < 0){
+				x = 59;
+			}
+			if(y > 30){
+				y = 0;
+			}
+			if(y < 0){
+				y = 30;
+			}
+			if(field[x][y] == 'Θ'){
+				levelup(level); // We will make this a function later.
+			} else {
+				field[x][y] = 'X';
+			}
+			if(s == 'q'){
+				s = 'Q';
+			}
+		}
+
+	system("stty cooked");
+	
+}
